6fc5db160bf81342afa2a59b2ca7b14e8f202400
Александр <durgen2014@gmail.com>
2023-01-04 02:10:49 +0300

    доработанная домашка

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -1,35 +1,34 @@
+import java.time.LocalDate;
 
-import java.util.Scanner;
-
-public class LeapYear {
+class Main {
     public static void main(String[] args) {
-        task1();
-        recomnendApplicationVersion(3, 2015);
-        calculateDeliveryDays (3);
-
-
+        printlsLeapYear(2023);
+        recomnendApplicationVersion(0, 2020);
+        calculateDeliveryDays(6);
     }
 
-    public static void task1() {
+
+    private static void printIsLeapYearResult(int year, boolean yearIsLeap) {
         System.out.println("Задача 1");
-        int year = 2023;
-        {
-            Scanner scan = new Scanner(System.in);
-            if ((year % 4 == 0) && year % 100 != 0) {
-                System.out.println(year + " Высокостный.");
-            } else if ((year % 4 == 0) && (year % 100 == 0)
-                    && (year % 400 == 0)) {
-                System.out.println(year + " Высокостный.");
-            } else {
-                System.out.println(year + " Не высокостный.");
-            }
+        if (yearIsLeap) {
+            System.out.println(year + " высокосный");
+        } else {
+            System.out.println(year + " не высокосны");
+
         }
+    }
+
+    public static void printlsLeapYear(int year) {
+        printIsLeapYearResult(year, isLeap(year));
+    }
+
+    private static boolean isLeap(int year) {
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
     }
 
     public static void recomnendApplicationVersion(int clietOS, int deviceYear) {
         System.out.println("Задача 2");
-        boolean deviceIsOld;
-        deviceIsOld = deviceIsOld(deviceYear);
+        boolean deviceIsOld = isDeviceOld(deviceYear);
         System.out.print("Установите ");
         if (deviceIsOld) {
             System.out.print("Lite ");
@@ -42,25 +41,22 @@ public class LeapYear {
         }
     }
 
-    private static boolean deviceIsOld(int deviceYear) {
-        return false;
+    private static boolean isDeviceOld(int deviceYear) {
+        int currentyear = LocalDate.now().getYear();
+        return deviceYear <= currentyear;
     }
 
-
-
-        public static String calculateDeliveryDays ( int deliveryDistance) {
-            System.out.println("Задача 3");
-            int deliveryDays = 1;
-            if (deliveryDays > 20) {
-                deliveryDays++;
-            }
-            if (deliveryDistance > 60 && deliveryDistance <= 100) {
-                deliveryDays++;
-            } else if (deliveryDistance > 100) {
-                return "Не возможно расчитать срок доставки ";
-            }
-            return "Потребуется дней: " + deliveryDays;
+    public static String calculateDeliveryDays(int deliveryDistance) {
+        System.out.println("Задача 3");
+        int deliveryDays = 1;
+        if (deliveryDays > 20) {
+            deliveryDays++;
         }
+        if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            deliveryDays++;
+        } else if (deliveryDistance > 100) {
+            return "Не возможно расчитать срок доставки ";
+        }
+        return "Потребуется дней: " + deliveryDays;
     }
-
-
+}
